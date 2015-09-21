ad0265fb91a76ab692d7ee44bfa42098fa2fa5ff
Jawad <hello@rzr.io>
2015-09-21 02:06:27 +0100

    remove /src/img ref in vendor/main . css files

--- frontend/views/page/view.php
+++ frontend/views/page/view.php
@@ -5,28 +5,8 @@
  */
 $this->title = $model->title;
 ?>
-    <nav id=menu class="menu hide">
-        <ul>
-            <li><span><?php echo $model->title ?></span></li>
-            <li><span></span></li>
-            <li><span></span></li>
-            <li><span></span></li>
-            <li id=homeAnchor class=active><span><b>RZR</b>.IO</span></li>
-        </ul>
-        <div id=menuItemActive class=item-active-bottom>
-            <div class="menu-handler left"></div>
-            <!--div class="menu-handler right"></div--></div>
-    </nav><!-- burguer menu and fullscreen nav -->
-    <div id=burguerMenu class="burguer-container hide">
-        <div id=burguerTitle class=burguer-title>::Home</div>
-        <a id=trigger-overlay class=burger href=#><span></span></a></div>
-    <div class=burguer-overlay>
-    <nav>
-        <ul>
-            <li><a id=aboutBurguerItem href=#><?php echo $model->title ?></a></li>
-        </ul>
-    </nav>
 
-<div class="description">
+<div style="color: white;">
+    <h1><?php echo $model->title ?></h1>
 <?php echo $model->body ?>
     </div>
